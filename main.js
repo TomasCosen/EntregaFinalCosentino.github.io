372a4a5dd582315f720af7e5e578edab1423cb48
Tomas Cosentino <tomascosentino123@gmail.com>
2023-08-22 02:00:08 -0300

    add historial

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let usuario
 let usuarioStorage = localStorage.getItem("usuarioActual")
+let historialValores = [];
+const historialElement = document.getElementById("historial")
 const contenedor = document.getElementById("contenedor");
 const boton = document.getElementById("boton");
+const cleanHistory = document.getElementById("cleanHistory");
 const numeroInput = document.getElementById("numero");
 const opcionSelect = document.getElementById("opcionSelect");
 const suma = (a, b, c, d, e) => a + b + c + d + e;
@@ -10,30 +13,6 @@ const paisServ = (x) => x * 0.08;
 const paisProd = (x) => x * 0.3;
 const regAfip = (x) => x * 0.45;
 const iibb = (x) => x * 0.02;
-
-
-// inicio de sesión
-
-if(usuarioStorage){
-    nombre = usuarioStorage
-    alert(`Bienvenido nuevamente ${nombre}`)
-}else{
-    let nombre = prompt("Ingrese el usuario")
-    localStorage.setItem("usuarioActual", nombre)
-    alert("Hola, Bienvenido al calculador de impuestos al dólar")
-}
-
-// cierre de sesión
-
-const logout = document.getElementById("logout")
-
-logout.addEventListener("click", () => {
-    localStorage.removeItem("usuarioActual")
-    location.reload()
-    alert("Sesión cerrada, hasta luego!")
-})
-
-// lista de valores comunes
 const valoresComunes = [
     { valor: 100 },
     { valor: 200 },
@@ -43,7 +22,80 @@ const valoresComunes = [
     { valor: 5000 },
     { valor: 10000 },
 ];
+const actualizarHistorial = () => {
+    historialElement.innerHTML = ""; // Limpiar el historial
 
+    // Agregar los valores calculados al historial
+    historialValores.forEach((valor, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `Resultado ${index + 1}: ARS$ ${valor}`;
+        historialElement.appendChild(listItem);
+    });
+};
+
+// inicio de sesión
+
+if(usuarioStorage){
+    nombre = usuarioStorage
+    alert(`Bienvenido ${nombre}`)
+}else{
+    let nombre = prompt("Ingrese el usuario")
+    localStorage.setItem("usuarioActual", nombre)
+    alert("Hola, Bienvenido al calculador de impuestos al dólar")
+    location.reload()
+}
+const historialUsuario = localStorage.getItem(`${nombre}-historial`);
+if (historialUsuario) {
+    historialValores = JSON.parse(historialUsuario);
+    actualizarHistorial()
+}
+
+// cierre de sesión
+
+const logout = document.getElementById("logout")
+
+logout.addEventListener("click", () => {
+    localStorage.removeItem("usuarioActual")
+    localStorage.removeItem(`${nombre}-historial`)
+    location.reload()
+    alert("Sesión cerrada, hasta luego!")
+})
+
+//borrado de historial
+cleanHistory.addEventListener("click", () => {
+    localStorage.removeItem(`${nombre}-historial`)
+    location.reload()
+})
+
+// Función para seleccionar el valor del array y calcular los impuestos
+const calcularImpuestos = (numero, opcionSelect) => {
+    let valor;
+    
+    // Validar y obtener el valor seleccionado
+    if (numero > 0 && numero <= valoresComunes.length) {
+        valor = valoresComunes[numero-1].valor;
+    } else {
+        valor = numero;
+    }
+
+    const opcion = opcionSelect.value;
+
+    // Calcular impuestos según la opción seleccionada
+    let resultado
+    if (opcion === "producto") {
+        const resultadoProd = suma(valor, regAfip(valor), paisProd(valor), 0, 0);
+        alert("El valor final en pesos para este producto es de: ARS$" + resultadoProd);
+        historialValores.push(resultadoProd)
+        
+    } else {
+        const resultadoServ = suma(valor, regAfip(valor), paisServ(valor), iva(valor), iibb(valor));
+        alert("El valor final en pesos para el servicio es de: ARS$" + resultadoServ);
+        historialValores.push(resultadoServ)
+    }
+    localStorage.setItem(`${nombre}-historial`, JSON.stringify(historialValores));
+    actualizarHistorial()
+
+};
 
 // Renderizar "valores comunes" en el contenedor
 const renderizado = (valoresComunes) => {
@@ -55,33 +107,11 @@ const renderizado = (valoresComunes) => {
     valoresList += " ó Ingrese el valor manualmente<br>";
     contenedor.innerHTML = valoresList;
 };
+
 renderizado(valoresComunes);
 
-// Función para seleccionar el valor del array
-const calcularImpuestos = (numero, opcionSelect) => {
-    let valor;
-    
-    // Validar y obtener el valor seleccionado
-    if (numero >= 0 && numero < valoresComunes.length) {
-        valor = valoresComunes[numero-1].valor;
-    } else {
-        valor = numero;
-    }
-
-    const opcion = opcionSelect.value;
-
-    // Calcular impuestos según la opción seleccionada
-    if (opcion === "producto") {
-        const resultadoProd = suma(valor, regAfip(valor), paisProd(valor), 0, 0);
-        alert("El valor final en pesos para este producto es de: ARS$" + resultadoProd);
-    } else {
-        const resultadoServ = suma(valor, regAfip(valor), paisServ(valor), iva(valor), iibb(valor));
-        alert("El valor final en pesos para el servicio es de: ARS$" + resultadoServ);
-    }
-};
-
-// Agregar evento al botón
 boton.addEventListener("click", () => {
     const numero = parseInt(numeroInput.value); // Obtener el número ingresado
     calcularImpuestos(numero, opcionSelect);
+    actualizarHistorial()
 });
